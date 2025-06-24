588c4e34c73b2adaa116dd68be74081b07281b6e
ashwetadesai <ashwetadesai3010@gmail.com>
2025-06-24 17:24:57 +0530

    removed some components

--- custom-grid-app/src/app/core/api.service.ts
+++ custom-grid-app/src/app/core/api.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, catchError, finalize, throwError } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class ApiService {
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+  loading$ = this.loadingSubject.asObservable();
 
-  constructor() { }
+  private API_URL = 'https://01.fy25ey01.64mb.io/';
+
+  constructor(private http: HttpClient) {}
+
+  getGridData() {
+    this.loadingSubject.next(true);
+    return this.http.get<any>(this.API_URL).pipe(
+      finalize(() => this.loadingSubject.next(false)),
+      catchError(err => {
+        console.error('API Error:', err);
+        return throwError(() => new Error('API fetch failed'));
+      })
+    );
+  }
 }
