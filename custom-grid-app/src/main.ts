c90c6a127c8395e96022666d45a24d4420c0a8e7
ashwetadesai <ashwetadesai3010@gmail.com>
2025-06-25 13:48:06 +0530

    added router

--- custom-grid-app/src/main.ts
+++ custom-grid-app/src/main.ts
@@ -2,5 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { routes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule)],
+}).catch(err => console.error(err));
